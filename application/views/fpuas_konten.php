08c7d3e76b05baa64d83259e974a1886d867be73
DESKTOP-IQRPE91\BAYU <bayu_wenger@ymail.com>
2022-06-20 14:48:36 +0700

    20062022

--- application/views/fpuas_konten.php
+++ application/views/fpuas_konten.php
@@ -1,4 +1,16 @@
- <?php 
+ <div class="box">
+        <div class="box-header with-border">
+          <!-- <h3 class="box-title">Title</h3> -->
+
+          <div class="box-tools pull-right">
+            <button type="button" class="btn btn-box-tool" data-widget="collapse" data-toggle="tooltip" title="" data-original-title="Collapse">
+              <i class="fa fa-minus"></i></button>
+            <button type="button" class="btn btn-box-tool" data-widget="remove" data-toggle="tooltip" title="" data-original-title="Remove">
+              <i class="fa fa-times"></i></button>
+          </div>
+        </div>
+        <div class="box-body">
+          <?php 
 if ($catar==null) {
   echo $catar;
   echo "<div class='alert alert-danger'>Data Tidak Ditemukan.</div>";
@@ -113,3 +125,10 @@ if ($catar==null) {
               </table>
 
               <?php } ?>
+        </div>
+        <!-- /.box-body -->
+       <!--  <div class="box-footer">
+          Footer
+        </div> -->
+        <!-- /.box-footer-->
+      </div>
