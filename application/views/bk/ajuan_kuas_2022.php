4598bbe7893cef8dadc88eb6a3d97a0b132697dd
DESKTOP-IQRPE91\BAYU <bayu_wenger@ymail.com>
2022-06-23 08:57:30 +0700

    23062022

--- application/views/bk/ajuan_kuas_2022.php
+++ application/views/bk/ajuan_kuas_2022.php
@@ -6,7 +6,7 @@
                 
                   <div class="box-body">
                   <div class="form-group">
-                      <form method="post" action="<?php echo base_url() ?>admin/exportp2021">
+                      <form method="post" action="<?php echo base_url() ?>bk/ajuan_kuas2022p">
                       <label> Pilih Program Studi
                     </label>
                     <select name="prodi" id="prodi" class="form-control" style="width:50%;">  
@@ -14,12 +14,16 @@
                     <option value="92401">D3 Ketatalaksanaan Pelayaran Niaga dan Kepelabuhanan</option>
                     <option value="92402">D3 Teknika</option>
                     <option value="92403">D3 Nautika</option>
-                    <option value="61201">S1 Transportasi</option>            
+                    <option value="61201">S1 Transportasi</option>  
+                    <option value="21207">S1 Teknik Transportasi Laut</option>
+                    <option value="21201">S1 Teknik Mesin</option>
+                    <option value="13241">S1 Teknik Keselamatan</option>
+                    <option value="94205">S1 Perdagangan Internasional</option>            
                     </select> 
                     </div>
                   <div class="form-group">
-                  <label>Tahun Lulus</label>
-                  <input type="text" class="form-control autocomplete" id="tahun_lulus" name="tahun_lulus" style="width: 40%;" placeholder="diisi dengan tahun lulus">
+                  <label>Tahun Masuk</label>
+                  <input type="text" class="form-control autocomplete" id="tahun_masuk" name="tahun_masuk" style="width: 40%;" placeholder="diisi dengan tahun lulus">
                   </div>
                     <div class="form-group">
                       <input type="submit" class="btn btn-block btn-success" id="cek" name="cek" style="width:20%;" value="Cek">
