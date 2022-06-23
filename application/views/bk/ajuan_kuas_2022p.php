5152126bbc0b88635c3c9fc1f049cdb59dc6d4c1
DESKTOP-IQRPE91\BAYU <bayu_wenger@ymail.com>
2022-06-23 13:42:33 +0700

    23062022

--- application/views/bk/ajuan_kuas_2022p.php
+++ application/views/bk/ajuan_kuas_2022p.php
@@ -14,7 +14,7 @@
               <a class="btn btn-success btn-sm" href="#"><i class="fa fa-check"></i></a>Kliring di ACC,
               <a class="btn btn-danger btn-sm" href="#"><i class="fa fa-close"></i></a>Kliring di TOLAK
               </div>
-              <table id="example1" class="table table-bordered table-striped">
+              <table id="example1" class="table table-bordered table-striped display nowrap">
                 <thead>
                 <tr>
                   <th>NIM</th>
