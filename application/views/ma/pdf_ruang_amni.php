8ebfdc05e0c3689fb674f0ecd3afb9b287bb127f
DESKTOP-IQRPE91\BAYU <bayu_wenger@ymail.com>
2022-08-23 14:08:35 +0700

    23082022

---
+++ application/views/ma/pdf_ruang_amni.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Cetak QRCODE Ruang</title>
+<style>
+  
+
+   figure {
+     border: 4px solid #575D63;
+     margin: 20px;
+     padding: 10px;
+     width: 95px;
+     height: 65px;
+    width: 25%;
+    float: left;
+    text-align: center;
+    padding: 0;
+    display: inline-block;
+    font-size: 10px;
+}
+</style>
+</head>
+<body>
+
+
+<table>
+  <?php 
+foreach ($qrcode as $q) {
+# code...
+?>
+  <tr>
+    <td rowspan="2"> <img width="20%" src="<?php echo base_url() ?>assets/1/images/amni-png.png"></td>
+    <td> UNIMAR AMNI </td>
+    <td rowspan="2">
+      <figure>
+      <img src="<?php echo base_url().'assets/aset/qrcode/'.$q->qrcode;?>">
+      <!-- <figcaption><?php //echo $q->nama."-".$q->nama_gedung."-".$q->nama_ruang."-".$q->nama_barang."-".$q->brg_ke." Ket :".$q->keterangan; ?></figcaption> -->
+      </figure>
+    </td>
+  </tr>
+  <tr>Keterangan</tr>
+  <?php 
+}
+ ?>
+</table>
+
+
+</body>
+</html>
