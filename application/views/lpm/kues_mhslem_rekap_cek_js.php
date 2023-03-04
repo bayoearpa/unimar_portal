0b362908cd3af7e9e8ae1871d29791e8de9a2a29
DESKTOP-IQRPE91\BAYU <bayuarpa@gmail.com>
2023-03-04 11:13:25 +0700

    04022023

--- application/views/lpm/kues_mhslem_rekap_cek_js.php
+++ application/views/lpm/kues_mhslem_rekap_cek_js.php
@@ -28,49 +28,7 @@
     });
   }
 
-<?php //foreach ($list_pert as $key) {?>
-      /*
-     * DONUT CHART
-     * -----------
-     */
 
-    var donutData<?php //echo $key->id_mhsdsn ?> = [
-     { label: 'STS', data: <?php //echo $lpm->countitem_persentase_mhsdsn($key->id_mhsdsn,$prodi,$ta,'1') ?>, color: '#f20b0b' },
-      { label: 'TS', data: <?php //echo $lpm->countitem_persentase_mhsdsn($key->id_mhsdsn,$prodi,$ta,'2') ?>, color: '#ffad5f' },
-      { label: 'KS', data: <?php//echo $lpm->countitem_persentase_mhsdsn($key->id_mhsdsn,$prodi,$ta,'3') ?>, color: '#ffd966' },
-      { label: 'S', data: <?php //echo $lpm->countitem_persentase_mhsdsn($key->id_mhsdsn,$prodi,$ta,'4') ?>, color: '#9af073' },
-      { label: 'SS', data: <?php //echo $lpm->countitem_persentase_mhsdsn($key->id_mhsdsn,$prodi,$ta,'5') ?>, color: '#89ddfc' }
-    ]
-    $.plot('#donut-chart-<?php //echo $key->id_mhsdsn ?>', donutData<?php //echo $key->id_mhsdsn; ?>, {
-        series: {
-          pie: { 
-            show: true,
-            radius: 1,
-            label: {
-              show: true,
-              radius: 3/4,
-              formatter: labelFormatter,
-              background: {
-                opacity: 0.5
-              }
-            }
-          }
-        },
-        legend: {
-          show: false
-        }
-      });
-
-
-   <?php //} ?>
-    /*
-     * END DONUT CHART
-     */
-     /*
-    
-   * Custom Label formatter
-   * ----------------------
-   */
   function labelFormatter(label, series) {
     return '<div style="font-size:13px; text-align:center; padding:2px; color: #fff; font-weight: 600;">'
       + label
