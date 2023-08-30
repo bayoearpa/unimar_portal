1bba85f97b057911d604d51acb4b5785940ea7c0
DESKTOP-IQRPE91\BAYU <bayuarpa@gmail.com>
2023-08-30 17:33:08 +0700

    30082023

--- application/views/baak/mon_llsd3_js.php
+++ application/views/baak/mon_llsd3_js.php
@@ -3,11 +3,12 @@
         $('#filter-form').submit(function(e) {
             e.preventDefault();
             var year = $('#year').val();
+            var programStudi = $('#program_studi').val();
 
             $.ajax({
                 type: 'GET',
                 url: '<?php echo base_url('baak/mon_llsd3'); ?>',
-                data: { year: year },
+                ddata: { year: year, program_studi: programStudi }, // Send both year and program_studi
                 success: function(response) {
                     $('#example1').html(response); // Ganti isi #item-list dengan hasil AJAX
                 }
