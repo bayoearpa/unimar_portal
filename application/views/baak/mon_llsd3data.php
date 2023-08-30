0f508558db18509f646b45af89ea61ef3d16d154
DESKTOP-IQRPE91\BAYU <bayuarpa@gmail.com>
2023-08-31 02:32:20 +0700

    31082023

---
+++ application/views/baak/mon_llsd3data.php
@@ -0,0 +1,41 @@
+  <table id="example1" class="table table-bordered table-striped">
+                <thead>
+                <tr>
+                  <th>NIM</th>
+                  <th>Nama</th>
+                  <th>Prodi</th>
+                  <!-- <th>Jenjang</th>
+                  <th>Mahatar</th>
+                  <th>D.Wali</th>
+                  <th>BAAK</th>
+                  <th>BK</th>
+                  <th>PPK</th>
+                  <th>Hasil</th> -->
+                </tr>
+                </thead>
+                <?php foreach ($items as $i) {
+                  # code...
+                 ?>
+                <tbody>
+                  <tr>
+                <td><?php echo $i->nim; ?></td>
+                <td><?php echo $i->nama; ?></td>
+                <td><?php echo $i->prodi; ?></td>
+                  </tr>
+                </tbody>
+              <?php } ?>
+                <tfoot>
+                <tr>
+                  <th>NIM</th>
+                  <th>Nama</th>
+                  <th>Prodi</th>
+                 <!--  <th>Jenjang</th>
+                  <th>Mahatar</th>
+                  <th>D.Wali</th>
+                  <th>BAAK</th>
+                  <th>BK</th>
+                  <th>PPK</th>
+                  <th>Hasil</th> -->
+                </tr>
+                </tfoot>
+              </table>
