aea7fbfefe8d4f92214222bf0015a490125a760d
DESKTOP-IQRPE91\BAYU <bayuarpa@gmail.com>
2023-08-30 16:40:46 +0700

    30082023

--- application/views/baak/mon_llsd3.php
+++ application/views/baak/mon_llsd3.php
@@ -40,9 +40,11 @@
                   # code...
                  ?>
                 <tbody>
+                  <tr>
                 <td><?php echo $i->nim; ?></td>
                 <td><?php echo $i->nama; ?></td>
                 <td><?php echo $i->prodi; ?></td>
+                  </tr>
                 </tbody>
               <?php } ?>
                 <tfoot>
