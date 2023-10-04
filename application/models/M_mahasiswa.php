59d4f4d2862e424b20a75fb1e08616550fc9874e
DESKTOP-IQRPE91\BAYU <bayuarpa@gmail.com>
2023-10-04 12:49:54 +0700

    04102023

--- application/models/M_mahasiswa.php
+++ application/models/M_mahasiswa.php
@@ -16,6 +16,18 @@ class M_mahasiswa extends CI_Model {
 		$this->db->where($where);
 		$this->db->update($table,$data);
 	}
+	function get_data_join_for_login($where){
+		$this->db->select('user_mhs.user,
+		user_mhs.pass,
+		tmst_mahasiswa.Nama_mahasiswa,
+		tmst_mahasiswa.Kode_program_studi');
+		$this->db->from('user_mhs');
+		$this->db->join('tmst_mahasiswa','user_mhs.user = tmst_mahasiswa.NIM','inner');
+		$this->db->where($where);
+		//$this->db->order_by('tbl_catar_validasi.no_reg', "asc");
+		$query=$this->db->get();
+		return $query;
+	}
 
 }
 
