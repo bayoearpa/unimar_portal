9448028de6246e4893a644a2eccfcd491961e9d4
DESKTOP-IQRPE91\BAYU <bayuarpa@gmail.com>
2023-10-04 15:24:16 +0700

    04102023

--- application/views/mahasiswa/header.php
+++ application/views/mahasiswa/header.php
@@ -145,7 +145,7 @@
                   <a href="#" class="btn btn-default btn-flat">Profile</a>
                 </div>
                 <div class="pull-right">
-                  <a href="<?php echo base_url().'bk/logout'; ?>" class="btn btn-default btn-flat">Sign out</a>
+                  <a href="<?php echo base_url().'mahasiswa/logout'; ?>" class="btn btn-default btn-flat">Sign out</a>
                 </div>
               </li>
             </ul>
