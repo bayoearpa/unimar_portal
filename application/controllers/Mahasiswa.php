6ee5a527ae3217581898092cab886c2b0e669f8d
DESKTOP-IQRPE91\BAYU <bayuarpa@gmail.com>
2023-10-04 12:50:26 +0700

    04102023

--- application/controllers/Mahasiswa.php
+++ application/controllers/Mahasiswa.php
@@ -30,44 +30,33 @@ class Mahasiswa extends CI_Controller {
 		if($this->form_validation->run() != false){
 			$where = array(
 				'user' => $username,
-				'password' => md5($password)			
+				'pass' => md5($password)			
 			);
-			$data = $this->m_portal->get_data($where,'tbl_admin');
-			$d = $this->m_portal->get_data($where,'tbl_admin')->row();
+			$data = $this->m_mahasiswa->get_data_join_for_login($where);
+			$d = $this->m_mahasiswa->get_data_join_for_login($where)->row();
 			$cek = $data->num_rows();
 			if($cek > 0){
 				$session = array(
-					'id_admin'=> $d->id_admin,
 					'user'=> $d->user,
 					'nama'=> $d->nama,
-					'level'=> $d->level,
+					'prodi'=> $d->Kode_program_studi,
 					'status' => 'login'
 				);
 				$this->session->set_userdata($session);
-				if ($d->level=='1') {
-						# code...
-						redirect(base_url().'mahatar/');
-					}elseif ($d->level=='2') {
-						# code...
-						redirect(base_url().'wdosen/');
-					}elseif ($d->level=='3'){
-						redirect(base_url().'ppk/');
-					}elseif ($d->level=='4'){
-						redirect(base_url().'baak/');
-					}
-					elseif ($d->level=='5'){
-						redirect(base_url().'bk/');
-					}elseif ($d->level=='6'){
-						redirect(base_url().'lpm/');
-					}elseif ($d->level=='7'){
-						redirect(base_url().'hrd/');
-					}
+				redirect(base_url().'mahasiswa/home');
 			}else{
 				redirect(base_url().'administrasi?pesan=gagal');			
 			}
 		}else{
 			$this->load->view('login');
 		}
+	}
+	public function home()
+	{
+		# code...
+		$this->load->view('baak/header');
+		$this->load->view('baak/home');
+		$this->load->view('baak/footer');
 	}
 
 }
