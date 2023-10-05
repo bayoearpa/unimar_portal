5f01c21c01ab0c66eb08ad06be287cad55070236
DESKTOP-IQRPE91\BAYU <bayuarpa@gmail.com>
2023-10-05 13:12:02 +0700

    05102023

--- application/views/mahasiswa/home.php
+++ application/views/mahasiswa/home.php
@@ -34,12 +34,9 @@
 
                 <h3 class="timeline-header"><a href="#">Perkuliahan</a></h3>
 
-               <!--  <div class="timeline-body">
-                  Etsy doostang zoodles disqus groupon greplin oooj voxy zoodles,
-                  weebly ning heekya handango imeem plugg dopplr jibjab, movity
-                  jajah plickers sifteo edmodo ifttt zimbra. Babblely odeo kaboodle
-                  quora plaxo ideeli hulu weebly balihoo...
-                </div> -->
+                <div class="timeline-body">
+                  Melakukan kegiatan perkuliahan di Universitas Maritim AMNI Semarang  
+                </div>
                <!--  <div class="timeline-footer">
                   <a class="btn btn-primary btn-xs">Read more</a>
                   <a class="btn btn-danger btn-xs">Delete</a>
@@ -56,8 +53,8 @@
 
                 <h3 class="timeline-header no-border"><a href="#">Prada</a></h3>
                   <div class="timeline-body">
-                    Prada dilaksanakan oleh Taruna Program Diploma Tiga di lingkungan UNIMAR AMNI
-                    yang telah memenuhi persyaratan sebagai berikut:
+                    <b>Prada dilaksanakan oleh Taruna Program Diploma Tiga di lingkungan UNIMAR AMNI
+                    yang telah memenuhi persyaratan sebagai berikut:</b>
                     <ol>
                     <li>Telah menyelesaikan kuliah sampai dengan semester V.</li>
                     <li>Telah selesai menempuh kuliah minimal 80 sks.</li>
@@ -65,8 +62,8 @@
                     <li>Mempunyai Indeks Prestasi Kumulatif (IPK) minimal 2,5.</li>
                     <li>Mempunyai nilai Test of English as a Foreign Language (TOEFL) minimal 400.</li>
                     </ol>
-                    Prosedur yang harus dilakukan oleh Taruna Program Diploma Tiga di lingkungan 
-                    UNIMARAMNI untuk pelaksanaan Prada adalah sebagai berikut:
+                    <b>Prosedur yang harus dilakukan oleh Taruna Program Diploma Tiga di lingkungan 
+                    UNIMARAMNI untuk pelaksanaan Prada adalah sebagai berikut:</b>
                     <ol>
                     <li>Minimal 2 (dua) bulan sebelumnya, taruna mengajukan surat booking space ke
                     perusahaan/instansi.</li>
@@ -93,12 +90,10 @@
 
                 <h3 class="timeline-header"><a href="#">Lulus D3</a></h3>
 
-                <!-- <div class="timeline-body">
-                  Take me to your leader!
-                  Switzerland is small and neutral!
-                  We are more like Germany, ambitious and misunderstood!
+                <div class="timeline-body">
+                  Telah selesai menempuh kegiatan perkuliahan D3.
                 </div>
-                <div class="timeline-footer">
+               <!--  <div class="timeline-footer">
                   <a class="btn btn-warning btn-flat btn-xs">View comment</a>
                 </div> -->
               </div>
@@ -109,7 +104,10 @@
               <i class="fa fa-user bg-red"></i>
 
               <div class="timeline-item">
-                <h3 class="timeline-header"><a href="#">Ujian Pra</a></h3>
+                <h3 class="timeline-header"><a href="#">Ujian Pra Prala</a></h3>
+                <div class="timeline-body">
+                  Ujian Pra Prala merupakan ujian kepelautan bagi Taruna Program Studi Diploma Tiga Teknika dan Program Studi Diploma Tiga Nautika telah menyelesaikan studinya sampai semester IV dengan ketentuan tidak ada nilai D pada mata kuliah yang akan diujikan dan melaksanakan ujian kepelautan ATT III / ANT III sebagai syarat melaksakan praktik laut (prala). Ujian Pra Prala dilaksanakan oleh Pelaksana Ujian Keahlian Pelaut (PUKP) 5 Semarang.
+                </div>
               </div>
             </li>
             <!-- END timeline item -->
@@ -129,6 +127,42 @@
               <div class="timeline-item">
 
                 <h3 class="timeline-header"><a href="#">Onboard</a></h3>
+                <div class="timeline-body">
+                  <b>Pelaksanaan Prala harus memenuhi persyaratan sesuai dengan ketentuan sebagai berikut:</b>
+                  <ol>
+                  <li> Dilaksanakan berlayar di kapal selama minimal 12 bulan 1 hari sesuai bidangnya
+                  (dibuktikan dengan Surat Pengalaman Berlayar yang dikeluarkan dari pihak yang
+                  berwenang).</li>
+                  <li>Dilaksanakan pada kapal niaga (bukan kapal perang, kapal negara, kapal pesiar pribadi atau
+                  kapal tradisional).</li> 
+                  <li>Dilaksanakan maksimum pada 2 (dua) kapal, di mana masing-masing kondite pada kapal
+                  tersebut sekurang-kurangnya harus “B”.</li> 
+                  <li>Dilaksanakan pada kapal dengan Gross Tons minimal 500 GT, untuk Program Studi
+                  Diploma Tiga Nautika.</li> 
+                  <li> Dilaksanakan pada kapal dengan Horse Power/Tenaga penggerak utama minimal 750 KW,
+                  untuk Program Studi Diploma Tiga Teknika.</li>
+                  <li>Pelaporan:</li> 
+                  <ol type="a">
+                  <li>Batas waktu taruna Prala mengirimkan laporan sign on ke kampus melalui email atau
+                  link/website yang sudah disiapkan oleh Bagian PPK adalah maksimal 1 (satu) bulan
+                  setelah taruna naik kapal.</li>
+                  <li>Selanjutnya setelah taruna naik kapal (sign onboard) secara rutin dan bertahap harus
+                  mengirimkan laporan tri wulan setiap 3 (tiga) bulan sekali yaitu di bulan ke 3, 6, 9, dan
+                  12 selama dalam masa praktik lautnya terhitung dari tanggal sign on nya.</li>
+                  <li>Apabila dalam batas waktu yang telah ditetapkan tidak ditaati, maka taruna yang
+                  bersangkutan dikenakan sanksi hukuman dan wajib melaksanakan sanksi hukuman
+                  sesuai ketentuan yang berlaku.</li>
+                  <li>Untuk batas waktu taruna laporan turun Prala adalah maksimal 15 (lima belas) hari 
+                  setelah tanggal sign off dengan melampirkan surat masa layar, surat sign on dan sign off 
+                  dari perusahaan tempat Prala, dan dokumen pendukung lainnya yang digunakan saat
+                  Prala.</li>
+                  <li>Jika dalam waktu yang telah ditetapkan, taruna yang bersangkutan tidak
+                  melaksanakakan sesuai ketentuan yang berlaku, maka taruna yang bersangkutan akan
+                  dikenakan sanksi hukuman dan wajib melaksanakan sanksi hukuman sesuai ketentuan
+                  yang berlaku.Pembimbingan Skripsi dan Karya Tulis serta Pengujian</li>
+                </ol>
+                  </ol>
+                </div>
               </div>
             </li>
             <!-- END timeline item -->
@@ -165,7 +199,10 @@
               <i class="fa fa-user bg-red"></i>
 
               <div class="timeline-item">
-                <h3 class="timeline-header no-border"><a href="#">Ujian Pasca</a></h3>
+                <h3 class="timeline-header no-border"><a href="#">Ujian Pasca Prala</a></h3>
+                <div class="timeline-body">
+                  Ujian Pasca Prala merupakan ujian kepelautan bagi Taruna Program Studi Diploma Tiga Teknika dan Program Studi Diploma Tiga Nautika yang melaksanakan ujian kepelautan ATT-III/ANT-III dan telah melaksanakan Prala selama 12 bulan. Ujian Pasca Prala dilaksanakan oleh PUKP 5 Semarang.
+                </div>
               </div>
             </li>
             <!-- END timeline item -->
