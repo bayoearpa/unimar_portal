c3d486ca4647010c87744f84020450771bed07e6
DESKTOP-IQRPE91\BAYU <bayuarpa@gmail.com>
2023-10-05 11:20:46 +0700

    05102023

--- application/views/mahasiswa/home.php
+++ application/views/mahasiswa/home.php
@@ -34,13 +34,13 @@
 
                 <h3 class="timeline-header"><a href="#">Perkuliahan</a></h3>
 
-                <!-- <div class="timeline-body">
+                <div class="timeline-body">
                   Etsy doostang zoodles disqus groupon greplin oooj voxy zoodles,
                   weebly ning heekya handango imeem plugg dopplr jibjab, movity
                   jajah plickers sifteo edmodo ifttt zimbra. Babblely odeo kaboodle
                   quora plaxo ideeli hulu weebly balihoo...
                 </div>
-                <div class="timeline-footer">
+               <!--  <div class="timeline-footer">
                   <a class="btn btn-primary btn-xs">Read more</a>
                   <a class="btn btn-danger btn-xs">Delete</a>
                 </div> -->
@@ -56,6 +56,32 @@
 
                 <h3 class="timeline-header no-border"><a href="#">Prada</a></h3>
               </div>
+               <div class="timeline-body">
+                  Prada dilaksanakan oleh Taruna Program Diploma Tiga di lingkungan UNIMAR AMNI
+                  yang telah memenuhi persyaratan sebagai berikut:
+                  <ul>
+                  <li>Telah menyelesaikan kuliah sampai dengan semester V.</li>
+                  <li>Telah selesai menempuh kuliah minimal 80 sks.</li>
+                  <li>Nilai mata kuliah sampai dengan semester IV tidak terdapat nilai D dan E.</li>
+                  <li>Mempunyai Indeks Prestasi Kumulatif (IPK) minimal 2,5.</li>
+                  <li>Mempunyai nilai Test of English as a Foreign Language (TOEFL) minimal 400.</li>
+                  </ul>
+                  Prosedur yang harus dilakukan oleh Taruna Program Diploma Tiga di lingkungan 
+                  UNIMARAMNI untuk pelaksanaan Prada adalah sebagai berikut:
+                  <ul>
+                  <li>Minimal 2 (dua) bulan sebelumnya, taruna mengajukan surat booking space ke
+                  perusahaan/instansi.</li>
+                  <li>Melaksanakan pembekalan Prada.</li>
+                  <li>Melaksanakan kliring dari Bagian Pembinaan Karakter Mahatar, Biro Administrasi Umum
+                  dan Keuangan (BAUK), Biro Administrasi Akademik Kemahataran (BAAK), Program
+                  Studi masing-masing dan Bagian PPK.</li>
+                  <li>Melaksanakan pemberkasan kliring siap Prada dan dilampiri fotocopy Kartu Hasil Studi
+                  (KHS) semester I sampai dengan IV.</li>
+                  <li>Mengajukan surat permohonan penempatan Prada.</li>
+                  <li>Melaksanakan Prada selama 4 (empat) bulan dengan dibekali Surat Penempatan Prada, 
+                  BukuSaku dan Conduite Nilai Prada.</li>
+                  </ul>
+                </div>
             </li>
             <!-- END timeline item -->
             <!-- timeline item -->
