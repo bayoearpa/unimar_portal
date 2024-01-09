1fb28a1d527fd5c7c353f333a12fa4a0aa143ee1
DESKTOP-IQRPE91\BAYU <bayuarpa@gmail.com>
2024-01-09 20:22:51 +0700

    09012024

--- application/views/fptpkl_cari_js.php
+++ application/views/fptpkl_cari_js.php
@@ -1,84 +1,86 @@
 <script>
-    $(document).ready(function() {
-        $('.view-filek-button').click(function(e) {
-            e.preventDefault();
-            var filename = $(this).data('filename');
-            var fileUrl = '/v1/assets/upload/tpkl/konduite/' + filename;
-            window.open(fileUrl, '_blank');
-        });
-
-        $('.view-filesk-button').click(function(e) {
-            e.preventDefault();
-            var filename = $(this).data('filename');
-            var fileUrl = '/v1/assets/upload/tpkl/sk/' + filename;
-            window.open(fileUrl, '_blank');
-        });
-
-        // Edit File Konduite button click event
-        $('.editfk-button').on('click', function(e) {
-            e.preventDefault();
-            console.log('Edit File Konduite button clicked');
-            var idTpkl = $(this).data('id-tpkl');
-            var nim = $(this).data('nim');
-            var formData = new FormData();
-            formData.append('id_tpkl', idTpkl);
-            formData.append('nim', nim);
-            formData.append('file_konduite', $('#file_konduite')[0].files[0]);
-            console.log('ID TPKL:', idTpkl, 'NIM:', nim);
-            editFileKonduite(formData);
-        });
-
-        // Edit File Surat Ket Off button click event
-        $('.editsk-button').on('click', function(e) {
-            e.preventDefault();
-            console.log('Edit File Surat Ket Off button clicked');
-            var idTpkl = $(this).data('id-tpkl');
-            var nim = $(this).data('nim');
-            formData.append('id_tpkl', idTpkl);
-            formData.append('nim', nim);
-            formData.append('file_suratketoff', $('#file_suratketoff')[0].files[0]);
-            console.log('ID TPKL:', idTpkl, 'NIM:', nim);
-            editFileSuratKetOff(formData);
-        });
-
-        // Function to handle editing File Konduite
-        function editFileKonduite(idTpkl, nim, formData) {
-            console.log('Editing File Konduite:', idTpkl, 'NIM:', nim);
-            $.ajax({
-                url: '<?php echo base_url('edit_file_konduite'); ?>',
-                type: 'POST',
-                processData: false,
-                contentType: false,
-                dataType: 'json',
-                data: formData,
-                success: function(response) {
-                    console.log('Edit File Konduite Response:', response);
-                    // Handle success (redirect or show a success message)
-                },
-                error: function(xhr, status, error) {
-                    console.error('Edit File Konduite Error:', error);
-                }
-            });
-        }
-
-        // Function to handle editing File Surat Ket Off
-        function editFileSuratKetOff(idTpkl, nim, formData) {
-            console.log('Editing File Surat Ket Off:', idTpkl, 'NIM:', nim);
-            $.ajax({
-                url: '<?php echo base_url('edit_file_sk'); ?>',
-                type: 'POST',
-                processData: false,
-                contentType: false,
-                dataType: 'json',
-                data: formData,
-                success: function(response) {
-                    console.log('Edit File Surat Ket Off Response:', response);
-                    // Handle success (redirect or show a success message)
-                },
-                error: function(xhr, status, error) {
-                    console.error('Edit File Surat Ket Off Error:', error);
-                }
-            });
-        }
+   $(document).ready(function() {
+    $('.view-filek-button').click(function(e) {
+        e.preventDefault();
+        var filename = $(this).data('filename');
+        var fileUrl = '/v1/assets/upload/tpkl/konduite/' + filename;
+        window.open(fileUrl, '_blank');
     });
+
+    $('.view-filesk-button').click(function(e) {
+        e.preventDefault();
+        var filename = $(this).data('filename');
+        var fileUrl = '/v1/assets/upload/tpkl/sk/' + filename;
+        window.open(fileUrl, '_blank');
+    });
+
+    // Edit File Konduite button click event
+    $('.editfk-button').on('click', function(e) {
+        e.preventDefault();
+        console.log('Edit File Konduite button clicked');
+        var idTpkl = $(this).data('id-tpkl');
+        var nim = $(this).data('nim');
+        var formData = new FormData();
+        formData.append('id_tpkl', idTpkl);
+        formData.append('nim', nim);
+        formData.append('file_konduite', $('#file_konduite')[0].files[0]);
+        console.log('ID TPKL:', idTpkl, 'NIM:', nim);
+        editFileKonduite(formData);
+    });
+
+    // Edit File Surat Ket Off button click event
+    $('.editsk-button').on('click', function(e) {
+        e.preventDefault();
+        console.log('Edit File Surat Ket Off button clicked');
+        var idTpkl = $(this).data('id-tpkl');
+        var nim = $(this).data('nim');
+        var formData = new FormData(); // Declare formData here
+        formData.append('id_tpkl', idTpkl);
+        formData.append('nim', nim);
+        formData.append('file_suratketoff', $('#file_suratketoff')[0].files[0]);
+        console.log('ID TPKL:', idTpkl, 'NIM:', nim);
+        editFileSuratKetOff(formData);
+    });
+
+    // Function to handle editing File Konduite
+    function editFileKonduite(formData) { // Removed idTpkl and nim from parameters
+        console.log('Editing File Konduite:', formData);
+        $.ajax({
+            url: '<?php echo base_url('edit_file_konduite'); ?>',
+            type: 'POST',
+            processData: false,
+            contentType: false,
+            dataType: 'json',
+            data: formData,
+            success: function(response) {
+                console.log('Edit File Konduite Response:', response);
+                // Handle success (redirect or show a success message)
+            },
+            error: function(xhr, status, error) {
+                console.error('Edit File Konduite Error:', error);
+            }
+        });
+    }
+
+    // Function to handle editing File Surat Ket Off
+    function editFileSuratKetOff(formData) { // Removed idTpkl and nim from parameters
+        console.log('Editing File Surat Ket Off:', formData);
+        $.ajax({
+            url: '<?php echo base_url('edit_file_sk'); ?>',
+            type: 'POST',
+            processData: false,
+            contentType: false,
+            dataType: 'json',
+            data: formData,
+            success: function(response) {
+                console.log('Edit File Surat Ket Off Response:', response);
+                // Handle success (redirect or show a success message)
+            },
+            error: function(xhr, status, error) {
+                console.error('Edit File Surat Ket Off Error:', error);
+            }
+        });
+    }
+});
+
 </script>
