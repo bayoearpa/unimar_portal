1f6be6a59c98dc65a6adc806dacc7c564ab879cc
DESKTOP-IQRPE91\BAYU <bayuarpa@gmail.com>
2024-01-24 10:45:35 +0700

    24012024

--- application/views/lpm/kues_mhsdsn_rekap_cek_js.php
+++ application/views/lpm/kues_mhsdsn_rekap_cek_js.php
@@ -21,6 +21,7 @@
     ]
 
   }
+  console.log(barChartData);
   window.onload = function(){
     var ctx = document.getElementById("canvas").getContext("2d");
     window.myBar = new Chart(ctx).Bar(barChartData, {
