36138dec679a6d5f22677bdf3e5a9242684312d3
DESKTOP-IQRPE91\BAYU <bayuarpa@gmail.com>
2024-04-23 15:53:17 +0700

    23042024

--- application/views/baak/header.php
+++ application/views/baak/header.php
@@ -267,7 +267,7 @@
 
           <ul class="treeview-menu">
             <li><a href="<?php echo base_url() ?>baak/mon_llsd3"><i class="fa fa-circle-o"></i> Lulus D3</a></li>
-            <li><a href="<?php echo base_url() ?>baak/mon_llspra"><i class="fa fa-circle-o"></i> Lulus UKP PRA</a></li>
+           <!--  <li><a href="<?php //echo base_url() ?>baak/mon_llspra"><i class="fa fa-circle-o"></i> Lulus UKP PRA</a></li> -->
            <!--  <li><a href="<?php //echo base_url() ?>baak/mon_sbprala"><i class="fa fa-circle-o"></i> SB Prala</a></li> -->
             <li><a href="<?php echo base_url() ?>baak/mon_llspasca"><i class="fa fa-circle-o"></i>Lulus UKP Pasca</a></li>
             <li><a href="<?php echo base_url() ?>baak/mon_summary"><i class="fa fa-circle-o"></i>Summary</a></li>
