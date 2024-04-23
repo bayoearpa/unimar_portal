dbcf540c854c21accebbd6d0a647ca5210044251
DESKTOP-IQRPE91\BAYU <bayuarpa@gmail.com>
2024-04-23 14:59:32 +0700

    23042024

--- application/views/baak/mon_llsd3_js.php
+++ application/views/baak/mon_llsd3_js.php
@@ -27,7 +27,8 @@
             });
 
  // Menampilkan modal saat tombol "Tambah" diklik
-  $('.add-button').click(function() {
+
+   $('#example31082023').on('click', '.add-button', function() {
     var id = $(this).data('id');
     // Ambil data yang akan diedit dari server dengan AJAX
     $.ajax({
@@ -56,7 +57,7 @@
     });
   });
 // Menampilkan modal saat tombol "Edit" diklik
-  $('.edit-button').click(function() {
+   $('#example31082023').on('click', '.edit-button', function() {
     var id = $(this).data('id');
     // Ambil data yang akan diedit dari server dengan AJAX
     $.ajax({
