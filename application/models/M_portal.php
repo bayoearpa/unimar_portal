737ce74d7c8fe83a04b473620f964da210dde888
DESKTOP-IQRPE91\BAYU <bayuarpa@gmail.com>
2024-06-28 10:31:30 +0700

    28062024

--- application/models/M_portal.php
+++ application/models/M_portal.php
@@ -1784,7 +1784,7 @@ function get_data_formon_mhs($id)
         $this->db->from('tbl_mon AS m');
         $this->db->join('tmst_mahasiswa AS s', 'm.nim = s.NIM', 'inner');
         $this->db->where('m.status_sb', 'iya');
-        //$this->db->where('m.pra_status', 'sudah');
+        $this->db->where('m.pra_status', 'sudah');
         $this->db->where('s.Kode_program_studi', $programStudi);
         $this->db->where('s.Tahun_masuk', $tahunMasuk);
         $query = $this->db->get();
