f6c462407f8ebfd325fe27ea1476208ae355768e
DESKTOP-IQRPE91\BAYU <bayuarpa@gmail.com>
2024-10-04 16:25:23 +0700

    04102024

--- application/views/ppk/mon_pencarianp.php
+++ application/views/ppk/mon_pencarianp.php
@@ -14,17 +14,17 @@ foreach ($results as $k) {
                 <!-- Post -->
                 <form class="form-horizontal">
                   <div class="form-group">
-                    <label for="inputName" class="col-sm-2 control-label">Nim</label>
+                    <label for="inputName" class="col-sm-4 control-label">Nim</label>
 
                     <div class="col-sm-10">
-                      <label for="inputName" class="col-sm-2 control-label"><?php echo $k->nim ?></label>
+                      <label for="inputName" class="col-sm-4 control-label"><?php echo $k->nim ?></label>
                     </div>
                   </div>
                   <div class="form-group">
-                    <label for="inputName" class="col-sm-2 control-label">Nama</label>
+                    <label for="inputName" class="col-sm-4 control-label">Nama</label>
 
                     <div class="col-sm-10">
-                      <label for="inputName" class="col-sm-2 control-label"><?php echo $k->nama ?></label>
+                      <label for="inputName" class="col-sm-4 control-label"><?php echo $k->nama ?></label>
                     </div>
                   </div>
                   </form>           
