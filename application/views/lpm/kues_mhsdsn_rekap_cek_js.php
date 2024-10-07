e4c4d729b7a3d3457f04560d69b81b65275e17e6
DESKTOP-IQRPE91\BAYU <bayuarpa@gmail.com>
2024-10-07 09:40:22 +0700

    07102024

--- application/views/lpm/kues_mhsdsn_rekap_cek_js.php
+++ application/views/lpm/kues_mhsdsn_rekap_cek_js.php
@@ -178,12 +178,12 @@
    * Custom Label formatter
    * ----------------------
    */
-  function labelFormatter(label, series) {
-    return '<div style="font-size:13px; text-align:center; padding:2px; color: #fff; font-weight: 600;">'
-      + label
-      + '<br>'
-      + Math.round(series.percent) + '%</div>'
-  }
+  // function labelFormatter(label, series) {
+  //   return '<div style="font-size:13px; text-align:center; padding:2px; color: #fff; font-weight: 600;">'
+  //     + label
+  //     + '<br>'
+  //     + Math.round(series.percent) + '%</div>'
+  // }
       
 
       </script>
