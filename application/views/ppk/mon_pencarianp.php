2f02dec9a7f53bc232b589a975696d81667b307d
DESKTOP-IQRPE91\BAYU <bayuarpa@gmail.com>
2024-10-08 14:00:37 +0700

    08102024

--- application/views/ppk/mon_pencarianp.php
+++ application/views/ppk/mon_pencarianp.php
@@ -303,38 +303,94 @@ foreach ($results as $k) {
                 <div class="box-body table-responsive no-padding">
               <table class="table table-hover">
                 <tbody><tr>
-                  <th>ID</th>
-                  <th>User</th>
-                  <th>Date</th>
+                  <th>Laporan Ke</th>
+                  <th>File</th>
+                  <th>Tanggal Upload</th>
                   <th>Status</th>
-                  <th>Reason</th>
+                  <th>Keterangan</th>
                 </tr>
                 <tr>
-                  <td>183</td>
-                  <td>John Doe</td>
+                  <td>Laporan Ke.1<td>
+                  <td>File</td>
                   <td>11-7-2014</td>
                   <td><span class="label label-success">Approved</span></td>
                   <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
                 </tr>
                 <tr>
-                  <td>219</td>
-                  <td>Alexander Pierce</td>
+                  <td>Laporan Ke.2<td>
+                  <td>File</td>
                   <td>11-7-2014</td>
-                  <td><span class="label label-warning">Pending</span></td>
+                  <td><span class="label label-success">Approved</span></td>
                   <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
                 </tr>
                 <tr>
-                  <td>657</td>
-                  <td>Bob Doe</td>
+                  <td>Laporan Ke.3<td>
+                  <td>File</td>
                   <td>11-7-2014</td>
-                  <td><span class="label label-primary">Approved</span></td>
+                  <td><span class="label label-success">Approved</span></td>
                   <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
                 </tr>
                 <tr>
-                  <td>175</td>
-                  <td>Mike Doe</td>
+                  <td>Laporan Ke.4<td>
+                  <td>File</td>
                   <td>11-7-2014</td>
-                  <td><span class="label label-danger">Denied</span></td>
+                  <td><span class="label label-success">Approved</span></td>
+                  <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
+                </tr>
+                <tr>
+                  <td>Laporan Ke.5<td>
+                  <td>File</td>
+                  <td>11-7-2014</td>
+                  <td><span class="label label-success">Approved</span></td>
+                  <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
+                </tr>
+                <tr>
+                  <td>Laporan Ke.6<td>
+                  <td>File</td>
+                  <td>11-7-2014</td>
+                  <td><span class="label label-success">Approved</span></td>
+                  <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
+                </tr>
+                <tr>
+                  <td>Laporan Ke.7<td>
+                  <td>File</td>
+                  <td>11-7-2014</td>
+                  <td><span class="label label-success">Approved</span></td>
+                  <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
+                </tr>
+                <tr>
+                  <td>Laporan Ke.8<td>
+                  <td>File</td>
+                  <td>11-7-2014</td>
+                  <td><span class="label label-success">Approved</span></td>
+                  <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
+                </tr>
+                <tr>
+                  <td>Laporan Ke.9<td>
+                  <td>File</td>
+                  <td>11-7-2014</td>
+                  <td><span class="label label-success">Approved</span></td>
+                  <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
+                </tr>
+                <tr>
+                  <td>Laporan Ke.10<td>
+                  <td>File</td>
+                  <td>11-7-2014</td>
+                  <td><span class="label label-success">Approved</span></td>
+                  <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
+                </tr>
+                <tr>
+                  <td>Laporan Ke.11<td>
+                  <td>File</td>
+                  <td>11-7-2014</td>
+                  <td><span class="label label-success">Approved</span></td>
+                  <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
+                </tr>
+                <tr>
+                  <td>Laporan Ke.12<td>
+                  <td>File</td>
+                  <td>11-7-2014</td>
+                  <td><span class="label label-success">Approved</span></td>
                   <td>Bacon ipsum dolor sit amet salami venison chicken flank fatback doner.</td>
                 </tr>
               </tbody></table>
