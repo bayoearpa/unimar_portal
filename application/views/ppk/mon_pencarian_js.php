87a4850531dc3bfb83773901de6e6b17917d4565
DESKTOP-IQRPE91\BAYU <bayuarpa@gmail.com>
2025-05-20 16:40:35 +0700

    20052025

--- application/views/ppk/mon_pencarian_js.php
+++ application/views/ppk/mon_pencarian_js.php
@@ -49,8 +49,8 @@ for (let i = 1; i <= 12; i++) {
       $.ajax({
         url: '<?php base_url('ppk/mon_laporanupdate') ?>', // Endpoint tujuan (di controller PPK, fungsi 'mon_laporanupdate')
         type: 'POST', // Metode HTTP POST
-        // data: $(this).serialize(), // Mengambil semua input form dan mengubahnya menjadi string query (name=value&...)
-        data: formData,
+        data: $(this).serialize(), // Mengambil semua input form dan mengubahnya menjadi string query (name=value&...)
+        // data: formData,
         
         success: function(response) {
           // Jika berhasil, tampilkan pesan sukses di elemen dengan id 'notifikasi'
