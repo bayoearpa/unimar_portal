4e0c049a9d8a2d3d8a52d9b5ebb875b6666298b5
DESKTOP-IQRPE91\BAYU <bayuarpa@gmail.com>
2025-06-11 10:38:00 +0700

    11062025

--- application/views/mahasiswa/offboard_js.php
+++ application/views/mahasiswa/offboard_js.php
@@ -18,6 +18,22 @@
             // Buka tautan ke file di jendela baru
             window.open(fileUrl, '_blank');
         });
+            $('.view-file-button3').click(function() {
+            var filename = $(this).data('filename');
+            // Gantilah '/uploads/' dengan direktori tempat Anda menyimpan file
+            var fileUrl = '/v1/assets/monitoring/offboard/' + filename;
+            
+            // Buka tautan ke file di jendela baru
+            window.open(fileUrl, '_blank');
+        });
+              $('.view-file-button4').click(function() {
+            var filename = $(this).data('filename');
+            // Gantilah '/uploads/' dengan direktori tempat Anda menyimpan file
+            var fileUrl = '/v1/assets/monitoring/offboard/' + filename;
+            
+            // Buka tautan ke file di jendela baru
+            window.open(fileUrl, '_blank');
+        });
 
 
     });
