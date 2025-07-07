e6a90c50720e0af440e3a5e0bdffc0dc8accaf87
DESKTOP-IQRPE91\BAYU <bayuarpa@gmail.com>
2025-07-07 14:17:44 +0700

    07072025

--- application/views/ppk/mon_modeling.php
+++ application/views/ppk/mon_modeling.php
@@ -4,7 +4,7 @@
         <div class="col-xs-12">
           <div class="box">
             <div class="box-header">
-              <h3 class="box-title">Prada</h3>
+              <h3 class="box-title">Modeling</h3>
             </div>
             <!-- /.box-header -->
             <div class="box-body">
