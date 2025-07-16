0c87f47fd4216662b1bac5ada80f8922fb4ec20b
DESKTOP-IQRPE91\BAYU <bayuarpa@gmail.com>
2025-07-16 09:28:07 +0700

    16072025

--- application/views/mahasiswa/diklat_tkbi.php
+++ application/views/mahasiswa/diklat_tkbi.php
@@ -83,7 +83,7 @@
                     </div>
                     <!-- end peringatan jika sudah pernah Mendaftar dan harus membayar  -->
                      <p align="center">Untuk Pembayaran dapat di Transfer ke Bank BTN dengan nomor rekening <b>0838810730 an. UNIMAR AMNI</b> biaya yang harus anda bayar sebesar:</p>
-                    <h2 align="center">Rp. 290.000,-</h2>
+                    <h2 align="center">Rp. 295.000,-</h2>
                     <form id="buktiBayarForm" method="post" enctype="multipart/form-data">
                     <div class="form-group">
                         <label for="editufsignon">Upload Bukti Bayar (file harus dengan format .pdf dan Maks. 1 MB)</label>
