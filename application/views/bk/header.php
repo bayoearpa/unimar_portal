09662528284d39c13973f5d4f9d39a35b65ee4bb
DESKTOP-IQRPE91\BAYU <bayuarpa@gmail.com>
2025-07-28 14:10:11 +0700

    28072025

--- application/views/bk/header.php
+++ application/views/bk/header.php
@@ -288,6 +288,7 @@
                 </li>
               </ul>
         </li>
+         <li><a href="<?php echo base_url() ?>bk/tkbi"><i class="fa fa-home"></i><span>TKBI</span></a></li>
 
       </ul>
     </section>
